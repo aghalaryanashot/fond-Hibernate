aee6a27ca61d87b6e04c9e8caee3aea115dadecf
Agalarian.AG <agalaryanashot@gmail.com>
2018-08-03 15:39:20 +0300

    adding a date filter

--- src/main/java/com/example/fond/service/PersonRepo.java
+++ src/main/java/com/example/fond/service/PersonRepo.java
@@ -1,10 +1,14 @@
 package com.example.fond.service;
 
 import com.example.fond.domain.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PersonRepo extends CrudRepository<Person,Long> {
     List<Person> findBylastName(String lastname);
+    @Query("select u from Person u where x.dateOfBirth between 1? and ?2")
+    Person findByStartDateBetween(Date startDate, Date between);
 }
