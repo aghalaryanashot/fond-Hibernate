b5929dc22d878d872b87d2f551b1791d512d1211
Ophir LOJKINE <o.lojkine@qwant.com>
2018-08-17 17:08:57 +0200

    add a test

--- src/test/java/com/example/fond/FondApplicationTests.java
+++ src/test/java/com/example/fond/FondApplicationTests.java
@@ -2,15 +2,35 @@ package com.example.fond;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class FondApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc
+                .perform(get("/filter1?firstName=invaliduser"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(containsString("Не найден пользователь")));
+    }
 }
